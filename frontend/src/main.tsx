c236557a8192d8ca579b9c67ed71169ab6a6642d
Sijal Manandhar <sijal.m06@gmail.com>
2025-07-11 15:06:49 +0545

    divided tailwind styles into chunks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "@/App";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import App from "@/App";
 import env from "@/config/env";
+import "@/styles/index.css";
 
 if (env.VITE_NODE_ENV === "production") {
   disableReactDevTools();
