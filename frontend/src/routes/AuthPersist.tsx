d2b7415441e9b0101a888f3c2ce0a849bfaf2fac
Sijal Manandhar <sijal.m06@gmail.com>
2025-10-10 05:05:43 +0545

    fix(auth): implement persistent toast management during data fetching

--- frontend/src/routes/AuthPersist.tsx
+++ frontend/src/routes/AuthPersist.tsx
@@ -1,12 +1,53 @@
-import { Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { sendRefreshTokenRequest } from "@/features/auth/slice";
 import Spinner from "@/components/common/Spinner";
 import { useAppDispatch } from "@/app/store/hooks";
+import { Id, toast } from "react-toastify";
 
 const AuthPersist = () => {
   const dispatch = useAppDispatch();
+  const location = useLocation();
   const [loading, setLoading] = useState(true);
+  const persistentToastIdRef = useRef<null | Id>(null);
+  const timerIdRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const isHomePage = useMemo(() => location.pathname === "/", [location.pathname]);
+
+  useEffect(() => {
+    if (!loading || isHomePage) {
+      if (timerIdRef.current) {
+        clearTimeout(timerIdRef.current);
+        timerIdRef.current = null;
+      }
+      if (persistentToastIdRef.current) {
+        toast.dismiss(persistentToastIdRef.current);
+        persistentToastIdRef.current = null;
+      }
+      toast.dismiss();
+      return;
+    }
+
+    const initialToastId = toast.info("Retrieving information.");
+    timerIdRef.current = setTimeout(() => {
+      toast.dismiss(initialToastId);
+      persistentToastIdRef.current = toast.info("This might take a moment.", {
+        autoClose: false,
+        position: "top-center",
+      });
+    }, 2000);
+
+    return () => {
+      if (timerIdRef.current) {
+        clearTimeout(timerIdRef.current);
+        timerIdRef.current = null;
+      }
+      if (persistentToastIdRef.current) {
+        toast.dismiss(persistentToastIdRef.current);
+        persistentToastIdRef.current = null;
+      }
+      toast.dismiss(initialToastId);
+    };
+  }, [isHomePage, loading]);
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -20,6 +61,10 @@ const AuthPersist = () => {
     };
     fetchToken();
   }, [dispatch]);
+
+  if (isHomePage) {
+    return <Outlet />;
+  }
 
   if (loading)
     return (
